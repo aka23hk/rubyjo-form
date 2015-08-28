782006f5805c48bd0a499d192fb3b3eed375d1b3
Akane <ne250128@senshu-u.jp>
2015-08-28 16:35:08 +0900

    入力内容が出るようにした

--- myapp.rb
+++ myapp.rb
@@ -5,3 +5,17 @@ get '/' do
 	@title = "入力フォーム"
     erb :index
 end
+
+get '/about' do
+  erb :about
+end
+
+get '/finish' do
+  erb :finish
+end
+
+post '/check' do
+  @email = params[:email]
+  @message = params[:message]
+  erb :check
+end
