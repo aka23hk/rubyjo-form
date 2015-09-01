56722146a042f8847abc0d6d45c92884c9692e0c
Akane <ne250128@senshu-u.jp>
2015-09-01 13:59:14 +0900

    だいぶすすんだw

--- myapp.rb
+++ myapp.rb
@@ -1,5 +1,9 @@
+require 'rubygems' # この辺を追記
+require 'bundler'
+Bundler.require
 require 'sinatra'
 require 'sinatra/reloader'
+require 'rest_client'
  
 get '/' do
 	@title = "入力フォーム"
@@ -7,15 +11,34 @@ get '/' do
 end
 
 get '/about' do
-  erb :about
-end
-
-get '/finish' do
-  erb :finish
+	erb :about
 end
 
 post '/check' do
-  @email = params[:email]
-  @message = params[:message]
-  erb :check
+	@email = params[:email]
+	@message = params[:message]
+	erb :check
+end
+
+post '/send_mail' do
+	@email = params[:email]
+	@message = params[:message]
+	send_simple_message
+	erb :finish
+end
+
+
+
+def greeting
+	"Hello Ruby"
+end
+
+
+def send_simple_message
+  RestClient.post "https://api:key-cabe4d08ae4cc3fab6b5946eb54f0123"\
+  "@api.mailgun.net/v3/sandboxcebbc7038ab145f1968fcd1e04eb9142.mailgun.org/messages",
+  :from => "Excited User <mailgun@sandboxcebbc7038ab145f1968fcd1e04eb9142.mailgun.org>",
+  :to => "akane <ne250128@senshu-u.jp>",
+  :subject => "Hello akane",
+  :text => "Congratulations akane, you just sent an email with Mailgun!  You are truly awesome!  You can see a record of this email in your logs: https://mailgun.com/cp/log .  You can send up to 300 emails/day from this sandbox server.  Next, you should add your own domain so you can send 10,000 emails/month for free."
 end
